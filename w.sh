a8faa9506b34f7bf0a1a092fd392fb1b20389d0f
jiyinyiyong <jiyinyiyong@gmail.com>
2012-09-28 10:21:39 +0800

    add git

--- w.sh
+++ w.sh
@@ -8,15 +8,14 @@ stylus -o self/css -w self/styl/global.styl &
 stylus -o self/css -w self/styl/index.styl &
 coffee -o self/js -w self/coffee/h.coffee &
 doodle self/css self/js self/jade &
-jade -O self/html -w self/jade/index.jade &
+jade -O self/html -wP self/jade/index.jade &
 
 read
 
-pkill -f 'node-dev/wrapper.js app.coffee'
-pkill -f 'stylus -o self/css -w self/styl/global.styl'
-pkill -f 'stylus -o self/css -w self/styl/index.styl'
-pkill -f 'coffee -o self/js -w self/coffee/h.coffee'
-pkill -f 'doodle self/css self/js self/jade'
-pkill -f 'jade -O self/html -w self/jade/index.jade'
+pkill -f 'node-dev'
+pkill -f 'stylus'
+pkill -f 'coffe'
+pkill -f 'doodle'
+pkill -f 'jade'
 
 echo '--- stop watching'
